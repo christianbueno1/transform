485822dfe5cbc3bee644a88da94d4ffe99c032a6
Christian Bueno <chmabuen@espol.edu.ec>
2021-08-10 15:04:21 -0500

    add father control of clip-path

--- scripts/transform2.js
+++ scripts/transform2.js
@@ -2,8 +2,9 @@ const app = {
     init: function(){
         // console.log(3);
         let inner2 = document.querySelector('.inner-clippath2');
-        // console.log(inner2);
-        inner2.addEventListener('click', e => {
+        let wrapperClippath = document.querySelector('.wrapper-clippath');
+        console.log(wrapperClippath);
+        wrapperClippath.addEventListener('click', e => {
             console.log('this', this);
             console.log('e.target', e.target);
             console.log('inner2', inner2);
