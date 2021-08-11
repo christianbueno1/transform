3c18fad08fec47252d2a8b9126cf281081a9cc92
Christian Bueno <chmabuen@espol.edu.ec>
2021-08-10 19:03:51 -0500

    add Ecuador animation

--- scripts/transform2.js
+++ scripts/transform2.js
@@ -1,14 +1,17 @@
 const app = {
     init: function(){
         // console.log(3);
-        let inner2 = document.querySelector('.inner-clippath2');
+        // let inner2 = document.querySelector('.inner-clippath2');
         let wrapperClippath = document.querySelector('.wrapper-clippath');
-        console.log(wrapperClippath);
-        wrapperClippath.addEventListener('click', e => {
+        let containerClippath = document.querySelector('.container-clippath');
+        containerClippath.addEventListener('click', e => {
             console.log('this', this);
             console.log('e.target', e.target);
-            console.log('inner2', inner2);
-            inner2.classList.toggle('action');
+            // console.log('inner2', inner2);
+            // containerClippath.classList.toggle('action');
+            wrapperClippath.classList.toggle('action');
+            // console.log(containerClippath);
+            console.log(wrapperClippath);
         });
     }
 }
